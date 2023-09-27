61d01569f016ce029077546743dbc775badaab70
Harinder <harrythind41@gmail.com>
2023-09-26 23:13:02 -0400

    feat: logo as link

--- src/components/shared/nav-bar/NavBar.jsx
+++ src/components/shared/nav-bar/NavBar.jsx
@@ -2,11 +2,12 @@ import './NavBar.css';
 import SocialLinks from "./social-links/SocialLinks";
 import NavLinkWrapper from "./nav-link-wrapper/NavLinkWrapper";
 import Menu from "./menu/Menu";
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div className='nav-bar'>
-            <div className='logo'></div>
+            <NavLink to='/' className='logo'></NavLink>
             <div className='nav-right-column'>
                 <div className='navlink-wrapper'><NavLinkWrapper /></div>
                 <div className='social-link-container'>
